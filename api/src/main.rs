c354c3d0b73206ed564e5cf5d115e989d2b31aba
Matěj Pavlíček <tikinang@gmail.com>
2025-06-14 12:15:39 +0200

    stream logs kickoff

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,10 +1,16 @@
 use actix_cors::Cors;
 use actix_files::Files;
-use actix_web::{post, put, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{post, put, web, App, Error, HttpResponse, HttpServer};
+// TODO: Can be replaced with std lib?
+use bytes::Bytes;
+// TODO: Can be replaced with std lib?
+use futures_util::stream::{self, StreamExt};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
+use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
-use std::{env, fs};
+use std::sync::mpsc;
+use std::{env, fs, thread};
 
 #[derive(Deserialize)]
 struct CompileQuery {
@@ -24,7 +30,10 @@ struct CompileResponse {
 }
 
 #[put("/compile")]
-async fn compile(body: web::Json<CompileBody>, query: web::Query<CompileQuery>) -> impl Responder {
+async fn compile(
+    body: web::Json<CompileBody>,
+    query: web::Query<CompileQuery>,
+) -> Result<HttpResponse, Error> {
     // TODO: Error handling.
 
     println!("workspace ID: {}", &query.workspace_id);
@@ -34,11 +43,9 @@ async fn compile(body: web::Json<CompileBody>, query: web::Query<CompileQuery>) 
         &query.workspace_id
     );
 
-    if !fs::exists(&path).unwrap() {
+    if !fs::exists(&path)? {
         // TODO: This is wrong and temporary. Users should not be allowed to use their own workspace IDs.
-        if let Err(err) = initialize_workspace(&query.workspace_id) {
-            return err;
-        };
+        initialize_workspace(&query.workspace_id)?;
     }
 
     fs::write(format!("{path}/src/lib.rs"), body.lib_rs.as_str()).unwrap();
@@ -46,29 +53,56 @@ async fn compile(body: web::Json<CompileBody>, query: web::Query<CompileQuery>) 
     fs::write(format!("{path}/index.html"), include_str!("user.html")).unwrap();
 
     println!("workspace PATH: {}", path);
-    match Command::new("trunk")
+    let mut child = Command::new("trunk")
         .current_dir(&path)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("build")
-        .status()
-    {
-        Err(error) => {
-            return HttpResponse::InternalServerError()
-                .body(format!("run trunk build: '{path}': {}", error.to_string()));
-        }
-        Ok(status) => {
-            if !status.success() {
-                return HttpResponse::InternalServerError()
-                    .body(format!("run trunk build: exit status: {:?}", status));
+        .spawn()
+        .map_err(|e| {
+            actix_web::error::ErrorInternalServerError(format!("failed to start trunk build: {e}",))
+        })?;
+
+    let stdout = child.stdout.take().ok_or_else(|| {
+        actix_web::error::ErrorInternalServerError("failed to capture trunk build stdout")
+    })?;
+    let stderr = child.stderr.take().ok_or_else(|| {
+        actix_web::error::ErrorInternalServerError("failed to capture trunk build stderr")
+    })?;
+
+    let (stdout_sender, receiver) = mpsc::channel();
+    let stderr_sender = stdout_sender.clone();
+
+    thread::spawn(move || {
+        let reader = BufReader::new(stdout);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                let _ = stdout_sender.send(line);
             }
         }
-    };
+    });
+    thread::spawn(move || {
+        let reader = BufReader::new(stderr);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                let _ = stderr_sender.send(line);
+            }
+        }
+    });
 
-    HttpResponse::Ok().json(CompileResponse {
-        js_download_url: format!("/workspaces/{}/dist/sheeet-lib.js", query.workspace_id),
-        wasm_download_url: format!("/workspaces/{}/dist/sheeet-lib_bg.wasm", query.workspace_id),
-    })
+    let stream = stream::unfold(receiver, |receiver| async move {
+        match receiver.recv() {
+            Ok(line) => Some((Ok(line), receiver)),
+            Err(_) => None, // Channel closed.
+        }
+    });
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/event-stream")
+        .streaming(stream.map(|result: Result<_, Error>| match result {
+            Ok(line) => Ok::<_, Error>(Bytes::from(format!("data: {}\n", line.trim()))),
+            Err(e) => Ok(Bytes::from(format!("data: Error: {}\n\n", e))),
+        })))
 }
 
 #[derive(Serialize)]
@@ -77,9 +111,7 @@ struct InitializeResponse {
 }
 
 #[post("/initialize")]
-async fn initialize() -> impl Responder {
-    // TODO: Error handling.
-
+async fn initialize() -> Result<HttpResponse, Error> {
     let workspace_id: String = rand::rng()
         .sample_iter(&rand::distr::Alphabetic)
         .take(12)
@@ -88,20 +120,20 @@ async fn initialize() -> impl Responder {
         .collect();
     println!("{}", workspace_id);
 
-    if let Err(err) = initialize_workspace(&workspace_id) {
-        return err;
-    };
+    initialize_workspace(&workspace_id)?;
 
-    HttpResponse::Ok().json(InitializeResponse { workspace_id })
+    Ok(HttpResponse::Ok().json(InitializeResponse { workspace_id }))
 }
 
-fn initialize_workspace(workspace_id: &str) -> Result<(), HttpResponse> {
+fn initialize_workspace(workspace_id: &str) -> Result<(), Error> {
     let path = format! {"{}/{}", env::var(ENV_KEY_API_WORKSPACES_PATH).unwrap_or(DEFAULT_WORKSPACES_PATH.into()), &workspace_id};
 
     match fs::create_dir_all(&path) {
         Err(error) => {
-            return Err(HttpResponse::InternalServerError()
-                .body(format!("create dir all '{path}': {}", error.to_string())));
+            return Err(actix_web::error::ErrorInternalServerError(format!(
+                "create dir all '{path}': {}",
+                error.to_string()
+            )));
         }
         _ => (),
     };
@@ -117,13 +149,17 @@ fn initialize_workspace(workspace_id: &str) -> Result<(), HttpResponse> {
         .status()
     {
         Err(error) => {
-            return Err(HttpResponse::InternalServerError()
-                .body(format!("run cargo init: '{path}': {}", error.to_string())));
+            return Err(actix_web::error::ErrorInternalServerError(format!(
+                "run cargo init: '{path}': {}",
+                error.to_string()
+            )));
         }
         Ok(status) => {
             if !status.success() {
-                return Err(HttpResponse::InternalServerError()
-                    .body(format!("run cargo init: exit status: {:?}", status)));
+                return Err(actix_web::error::ErrorInternalServerError(format!(
+                    "run cargo init: exit status: {:?}",
+                    status
+                )));
             }
         }
     };
